cc5a1ac72d5b7eb77b69a6b1654ee5fbc86a2c28
math <mayh.kokjo@gmail.com>
2021-03-19 19:05:32 -0500

    initial test cases

---
+++ src/main/java/com/javacircle/unit/asserts/Phone.java
@@ -0,0 +1,4 @@
+package com.javacircle.unit.asserts;
+
+public class Phone {
+}
--- src/test/java/com/javacircle/unit/asserts/PhoneTest.java
+++ src/test/java/com/javacircle/unit/asserts/PhoneTest.java
@@ -1,5 +1,12 @@
 package com.javacircle.unit.asserts;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneTest {
 
+    void phoneNumberMustHave10Digit(){
+        Phone  phone =  new Phone();
+        String phoneNum = "5154227301";
+        fail("a failing test");
+    }
 }

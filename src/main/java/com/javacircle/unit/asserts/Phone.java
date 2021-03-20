067c8a6cad27ec2cf8377e88700b3eec07ff3aa0
math <mayh.kokjo@gmail.com>
2021-03-19 20:59:52 -0500

    add test cases

--- src/main/java/com/javacircle/unit/asserts/Phone.java
+++ src/main/java/com/javacircle/unit/asserts/Phone.java
@@ -1,4 +1,34 @@
 package com.javacircle.unit.asserts;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Phone {
+
+	private static final String PATTERN = "\\d{10}";
+
+	public Phone() {}
+
+	public boolean isValidPhoneNumber(String phoneNum) {
+		Pattern pattern = Pattern.compile(PATTERN);
+		Matcher matcher = pattern.matcher(phoneNum);
+		return matcher.matches();
+	}
+
+
+	public  Phone(long number) throws Exception {
+		if(String.valueOf(number).length()!=0) {
+			throw new Exception(" Phone number must have 10 digit, lenght is "+ String.valueOf(number).length());
+		}	
+	}
+
+
+	public  void PhoneThrowsExample(long number) throws Exception {
+		if(String.valueOf(number).length()!=0) {
+			throw new Exception(" Phone number must have 10 digit, lenght is "+ String.valueOf(number).length());
+		}	
+
+
+	}
+
 }
--- src/test/java/com/javacircle/unit/asserts/PhoneTest.java
+++ src/test/java/com/javacircle/unit/asserts/PhoneTest.java
@@ -1,12 +1,42 @@
 package com.javacircle.unit.asserts;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class PhoneTest {
 
-    void phoneNumberMustHave10Digit(){
-        Phone  phone =  new Phone();
-        String phoneNum = "5154227301";
-        fail("a failing test");
-    }
+	@Test
+	void ValidPhoneNumberIs10Digit(){
+		Phone  phone =  new Phone();
+		String phoneNum = "1234567890";
+		assertTrue(phone.isValidPhoneNumber(phoneNum));
+	}
+
+	@Test
+	void InvalidPhoneNumberIsNot10Digit(){
+		Phone  phone =  new Phone();
+		String phoneNum = "123456789";
+		assertFalse(phone.isValidPhoneNumber(phoneNum));
+	}
+
+
+
+	@Test
+	void InvalidPhoneNumberValidThrowException(){
+		Long phoneNum = 12356l;
+		Exception exception =  assertThrows(Exception.class, ()->new  Phone(phoneNum));
+	}
+	
+	
+	@Test
+	@DisplayName("Custom test name containing spaces")
+	public void wrongInput() {
+		Phone  phone =  new Phone();
+		Long phoneNum = 0l;
+	    assertThrows(Exception.class,()->{phone.PhoneThrowsExample(phoneNum);} );
+	}
+
 }
